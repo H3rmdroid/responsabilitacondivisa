54b0d6cb9aa4ea26c3d147b4fa242f34a4207b00
Altanorth <ermapache@users.noreply.github.com>
2026-01-16 09:37:20 +0100

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  
 
   export default defineConfig({
+    base: "/responsabilitacondivisa/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
